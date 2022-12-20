1d3619634155ec1514dab0d2137a7dd88226614c
HoLLds <andreyka225588@gmail.com>
2022-12-20 23:46:51 +0300

    Proj

---
+++ Config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define DEFAULT_PEOPLE_LIMIT 15
+#define MOUNTED_INSIDE
+
+/***   WiFi   ***/
+//#define AP_MODE Does not work reliably
+#define STASSID "Test"
+#define STAPSK  "22222222"
+
+/***   Debugging Mode   ***/
+//#define DEBUG_COUNTER
+
+#endif
